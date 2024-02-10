2691b8acd44e66f3a1feda6e447c0b3452815178
vijay kumar <vijaykumarpalaparthi543@example.com>
2024-02-10 13:49:39 +0530

    added:CompareTo Method functionality

--- LineComCompProg.java
+++ LineComCompProg.java
@@ -3,6 +3,9 @@ package com.bridgelabz;
 public class LineComCompProg {
 	private double x1, y1, x2, y2;
 
+	
+	
+
 	public LineComCompProg(double x1, double y1, double x2, double y2) {
 		this.x1 = x1;
 		this.y1 = y1;
@@ -27,7 +30,23 @@ public class LineComCompProg {
 		LineComCompProg other = (LineComCompProg) obj;
 		return Double.compare(other.calculateLength(), this.calculateLength()) == 0;
 	}
+	private int value; // Length value
 
+    public LineComCompProg(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int compareTo(LineComCompProg length5) {
+        // Compare the lengths based on their values
+        return Integer.compare(this.value, length5.getValue());
+    }
+
+	
+	
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Line" + "Comparison Computation" + "Program on Master Branch");
@@ -53,6 +72,25 @@ public class LineComCompProg {
 
 		System.out.println("Are length1 and length2 equal? " + length1.equals(length2));
 		System.out.println("Are length1 and length3 equal? " + length1.equals(length3));
+
+		System.out.println("UC3");
+		// ---------------------------- UC3 -------------------------------------
+
+		LineComCompProg length4 = new LineComCompProg(17); // Length of 5 units
+		LineComCompProg length5 = new LineComCompProg(74); // Length of 7 units
+
+		int comparisonResult = length4.compareTo(length5);
+
+		if (comparisonResult < 0) {
+			System.out.println("Length 4 is shorter than Length 5");
+		} else if (comparisonResult > 0) {
+			System.out.println("Length 4 is longer than Length 5");
+		} else {
+			System.out.println("Length 4 is not equal to Length 5");
+		}
+
 	}
+
+
 
 }
